ce64e03613f4295e1f5db4e1019d74f4d049c7ee
Tomi <pintertamas99@gmail.com>
2025-05-28 10:29:06 +0200

    refactor(ecs): remove unused environment variables from ECS task definition

--- infra/terraform/ecs/ecs_task.tf
+++ infra/terraform/ecs/ecs_task.tf
@@ -19,10 +19,6 @@ resource "aws_ecs_task_definition" "game_task" {
       environment = [
         { name = "GAME_SESSION_ID", value = "" }, # override in Lambda
         { name = "USER_ID", value = "" }, # override in Lambda
-        { name = "COGNITO_DOMAIN", value = var.cognito_domain },
-        { name = "AWS_REGION", value = var.aws_region },
-        { name = "CLIENT_ID", value = var.cognito_client_id },
-        { name = "CALLBACK_URL", value = var.callback_url },
         { name = "IDLE_TIMEOUT_MINUTES", value = tostring(var.idle_timeout_minutes) }
       ]
     }
