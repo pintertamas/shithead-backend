518a4ed8ee6c84022f9c15eddc2e844e6fa80693
Tomi <pintertamas99@gmail.com>
2025-05-29 11:20:27 +0200

    fix(terraform): format output and variable definitions for consistency

--- infra/terraform/api_gateway/variables.tf
+++ infra/terraform/api_gateway/variables.tf
@@ -1,25 +1,25 @@
 variable "project_name" {
-    description = "Name of the project"
-    type        = string
+  description = "Name of the project"
+  type        = string
 }
 
 variable "create_game_invoke_arn" {
-    description = "Invoke ARN of the create game Lambda function"
-    type        = string
+  description = "Invoke ARN of the create game Lambda function"
+  type        = string
 }
 
 variable "create_game_function_name" {
-    description = "Name of the create game Lambda function"
-    type        = string
+  description = "Name of the create game Lambda function"
+  type        = string
 }
 
 variable "cognito_authorizer_id" {
-    description = "ID of the Cognito Authorizer for API Gateway"
-    type        = string
+  description = "ID of the Cognito Authorizer for API Gateway"
+  type        = string
 }
 
 variable "stage_name" {
-    description = "Stage name for the API Gateway"
-    type        = string
-    default     = "prod"
+  description = "Stage name for the API Gateway"
+  type        = string
+  default     = "prod"
 }
